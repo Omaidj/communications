b1609ea40b8841ba75b3c7e650de14c60a3d3fec
Omaid Jlilzad <93329170+Omaidj@users.noreply.github.com>
2023-04-26 22:55:50 +0100

    Unit Test Update

--- UnitTest1/UnitTest1.cs
+++ UnitTest1/UnitTest1.cs
@@ -32,12 +32,12 @@ namespace UnitTest1
         }
     }
 
-    [TestClass]
-public class MessageRepositoryTest
-{
-    [TestMethod]
-    public async Task TestSendMessageAsync()
-    {
+   [TestClass]
+   public class MessageRepositoryTest
+   {
+        [TestMethod]
+        public async Task TestSendMessageAsync()
+        {
         var connection = new SQLiteAsyncConnection(":memory:");
         var repository = new MessageRepository(connection);
         var message = new Message
@@ -54,31 +54,33 @@ public class MessageRepositoryTest
         Assert.IsNotNull(messages);
         Assert.AreEqual(1, messages.Count);
         Assert.AreEqual("Hello world!", messages[0].Text);
-    }
-
-        [TestMethod]
-        public async Task TestUpdateMessageAsync()
-        {
-            var connection = new SQLiteAsyncConnection(":memory:");
-            var repository = new MessageRepository(connection);
-            var message = new Message
-            {
-                SenderId = 1,
-                ReceiverId = 2,
-                Text = "Hello world!",
-                Timestamp = DateTime.Now
-            };
-            await repository.SendMessageAsync(message);
-
-            message.Text = "Updated message";
-            bool result = await repository.UpdateMessageAsync(message);
-            var updatedMessage = await repository.GetConversationAsync(1, 2);
-
-            // Assert
-            Assert.IsTrue(result);
-            Assert.AreEqual(1, updatedMessage.Count);
-            Assert.AreEqual("Updated message", updatedMessage[0].Text);
         }
+
+        //[TestMethod]
+        //public async Task TestUpdateMessageAsync()
+        //{
+            
+            //var connection = new SQLiteAsyncConnection(":memory:");
+            //var repository = new MessageRepository(connection);
+            //var message = new Message
+            //{
+            //    SenderId = 1,
+            //    ReceiverId = 2,
+            //    Text = "Hello world!",
+            //    Timestamp = DateTime.Now
+            //};
+            //await repository.SendMessageAsync(message);
+            //
+            //
+            //message.Text = "Updated message";
+            //bool result = await repository.UpdateMessageAsync(message);
+            //var updatedMessage = await repository.GetConversationAsync(1, 2);
+            //
+            //// Assert
+            //Assert.IsTrue(result);
+            //Assert.AreEqual(1, updatedMessage.Count);
+            //Assert.AreEqual("Updated message", updatedMessage[0].Text);
+        //}
     }
 
-    }
+}
